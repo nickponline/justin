843cac7bece300e6e875f099b0abe96785e43d88
Nicholas Pilkington <nick@dronedeploy.com>
2021-12-04 16:30:18 -0800

    Adds anagram

---
+++ anagram/main.py
@@ -0,0 +1,22 @@
+import collections
+
+def word_hash():
+
+    """"Create a hash table of the 7 letter words with the key as the
+    sorted(alphabetically) word and the value the word or words """
+
+    sorted_dict = collections.defaultdict(list)
+    words = [ x.strip() for x in open("dictionary.txt", "r") if len(x.strip()) == 7 ]
+
+    for x in words:
+        sorted_dict["".join(sorted(x))].append(x)
+
+    return sorted_dict
+
+if __name__ == '__main__':
+
+    test = ['RNOSVEI','RUIMOFN','ISCLINO','GUNEENI']
+    test1 = word_hash()
+    for word in test:
+        ana_list = test1["".join(sorted(word.lower()))]
+        print(f"{word} anagrams are : {ana_list}")
